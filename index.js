55f72e1005e2474ab3fc587cb8ad0b9f757455dc
khamlar <khamlar_chanthavong@yahoo.com>
2022-11-23 11:21:07 +0700

    uplasdasdas

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-header', 'Origin, X-Requested-With, Content-Type, Accrpt');
+    next();
+});
 
 
 app.get('/', async (req, res) => {
