a2590b69e2b42d1c7cc5998b85a676b810e09f30
khamlar <khamlar_chanthavong@yahoo.com>
2022-11-23 09:48:19 +0700

    asdasdasd

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-const api = require('./controllers/index')
+
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', async (req, res) => {
     res.status(200).json('api working');
 });
 
+const api = require('./controllers/index');
 app.use('/api', api);
 
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
+
+module.exports = app;
