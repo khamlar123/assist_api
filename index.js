7cf0c22a3e0be00c2ae668468d55b170a55702b9
khamlar <khamlar_chanthavong@yahoo.com>
2022-11-23 09:42:15 +0700

    asdasdasdasd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-const api = require('./controllers')
+const api = require('./controllers/index')
 app.use(cors());
 app.use(express.json());
 
